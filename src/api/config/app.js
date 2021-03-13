c35b89056c80bc300f9048d6afa71cd69628a6c8
kevin <kevinbermudezmejia@gmail.com>
2021-03-13 14:19:09 -0500

    add configuration for express: server listening, cors, express json

---
+++ src/api/config/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const responseHttp = require('../utils/responseHttp')
+const cors = require('cors')
+
+const app = express()
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    allowedHeaders:
+      'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  })
+)
+//middleware parser json requests
+app.use(express.json())
+
+//ROUTES_V1(app)
+
+//si no coincide con ninguna ruta definida pasa por acá
+app.use((req, res, next) => {
+  return responseHttp(res, 404)
+})
+
+module.exports = app
